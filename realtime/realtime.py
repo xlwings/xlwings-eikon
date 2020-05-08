a0566239bb2f2ef4210c7f533c9cf6f128621568
Felix Zumstein <fzumstein@gmail.com>
2020-05-08 14:03:17 +0200

    print some info

--- realtime/realtime.py
+++ realtime/realtime.py
@@ -12,6 +12,7 @@ warnings.filterwarnings("ignore")
 # Eikon setup
 conf = ConfigParser()
 conf.read(os.path.join(os.path.dirname(__file__), '..', 'eikon.conf'))
+print('Connecting to Eikon')
 ek.set_app_key(conf['eikon']['APP_KEY'])
 
 
@@ -19,10 +20,13 @@ def main():
     sheet = xw.Book.caller().sheets[0]
     rng = sheet['A2'].expand()
     instruments, fields = rng[1:, 0].value, rng[0, 1:].value
+    print('Connecting to streaming API')
     streaming_prices = ek.StreamingPrices(instruments=instruments, fields=fields)
     streaming_prices.open()
+    print('Start pushing to Excel')
 
     while True:
+        # Throttling to every half second
         rng[1, 1].value = streaming_prices.get_snapshot().values[:, 1:]
         sheet['B1'].value = dt.datetime.now()
         time.sleep(0.5)
